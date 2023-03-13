1a511ce543cec69246d1b1a6025fc0cfa508dbd7
wzseba <wzseba@gmail.com>
2023-03-12 23:35:08 -0300

    se agrego funcion edit

--- src/features/pacientes/pacienteSlice.js
+++ src/features/pacientes/pacienteSlice.js
@@ -1,22 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = [
-    {
-        id:"1",
-        name:"pedro",
-        sintomas:"le duele la panza"
-    },
-    {
-        id:"2",
-        name:"jose",
-        sintomas:"le duele la muela"
-    }
-]
+const initialState = []
 
 export const pacienteSlice = createSlice({
     name: 'pacientes',
     initialState,
-    reducers:{}
+    reducers:{
+        addPatient: (state, action) => {
+            state.push(action.payload)
+        },
+        deletePatient: (state, action) =>{
+            const patienteFound = state.find(paciente => paciente.id === action.payload)
+            if(patienteFound){
+                state.splice(state.indexOf(patienteFound),1)
+            }
+        },
+        editPatient: (state, action) =>{
+            const {id, name, email, telefono, sintomas} = action.payload
+            const editPatientFound = state.find(paciente => paciente.id === id)
+            if(editPatientFound){
+                editPatientFound.name = name,
+                editPatientFound.email = email,
+                editPatientFound.telefono = telefono,
+                editPatientFound.sintomas = sintomas
+            }
+        }
+    }
 })
+
+export const { addPatient, deletePatient, editPatient } = pacienteSlice.actions
 
 export default pacienteSlice.reducer
