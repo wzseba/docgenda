5e3c7b303581795ff65a5f32772a9fe17c6e4553
wzseba <wzseba@gmail.com>
2023-03-12 23:34:24 -0300

    se modifica estructura

--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -1,13 +1,49 @@
 import { useSelector } from "react-redux"
+import { Link } from "react-router-dom";
+import Pacientes from "../components/Pacientes"
+
 
 const Index = () => {
 
-  return (
-    <div>
-      <h1>Pacientes de hoy</h1>
-      <p>componente ordenamiento</p>
-      <p>compoente de busqueda</p>
-    </div>
+ const pacientes = useSelector(state => state.paciente);
+  
+  return ( 
+
+    <>
+    <h1 className="font-black text-4xl text-blue-800">Pacientes</h1>
+    <p className="mt-3">Administra tus pacientes</p>
+
+    {
+      pacientes?.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-gray-700 text-white">
+            <tr>
+              <th className="p-2">Paciente</th>
+              <th className="p-2">Contacto</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              pacientes.map( paciente => (
+                
+                  <Pacientes
+                    paciente={paciente}
+                    key={paciente.id}
+                  />
+                
+                
+              ))
+            }
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10">No hay pacientes aun</p>
+      )
+    }
+  
+  </>
+
   )
 }
 
