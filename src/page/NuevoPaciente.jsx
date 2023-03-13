e9b27cc0220ea6eaf77da8b1a92b92c29efb4eb1
wzseba <wzseba@gmail.com>
2023-03-12 23:36:00 -0300

    se borro componente pacientes modificacion de estructura

--- src/page/NuevoPaciente.jsx
+++ src/page/NuevoPaciente.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Formulario from '../components/Formulario'
 import Pacientes from '../components/Pacientes'
 
 
 const NuevoPaciente = () => {
+  const paciente = useSelector(state => state.paciente);
+  
   return (
     <>
       <h1 className='font-black text-4xl text-blue-900'>Ingresar Paciente</h1>
       <p className='mt-3'>Completar todos los campos para registrar paciente</p>
     
-      <div className='mt-12 md:flex'>
-        <Formulario/>
-        <Pacientes/>
-      </div>
-      
+      <Formulario/>
+    
     </>
     
   )
