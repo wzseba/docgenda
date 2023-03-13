35d80ad93c77e7e3538a6a8a5edb6be80a695a97
wzseba <wzseba@gmail.com>
2023-03-12 23:32:49 -0300

    se corrigio estructura y estilos

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,28 +1,41 @@
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom";
+import { deletePatient, editPatient } from "../features/pacientes/pacienteSlice";
 
+const Pacientes = ({paciente}) => {
 
-const Pacientes = () => {
+  const navigate = useNavigate();
+  
+  const { name, sintomas, email, telefono, id} = paciente
 
-  return (
-    <div className='md:w-1/2 lg:w-3/5 h-72 mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-xl'>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Nombre: <span className='font-normal normal-case'></span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Email: <span className='font-normal normal-case'></span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Fecha Alta: <span className='font-normal normal-case'></span></p>
-        <p className='font-bold mb-3 text-gray-700 uppercase'>Sintomas: <span className='font-normal normal-case'></span></p>
-
-        <div className="flex justify-between mt-5">
+return (
+  <tr className="border-b">
+      <td className="p-6 space-y-2 text-center">
+          <p className="text-2xl text-gray-800">{name}</p>
+          
+      </td>
+      <td className="p-6 text-center">
+          <p className="text-gray-600"> <span className="text-gray 800 uppercase font-bold">Email: </span>{email} </p>
+          <p className="text-gray-600"> <span className="text-gray 800 uppercase font-bold">Tel: </span>{telefono} </p>
+      </td>
+      <td className="p-6 flex justify-evenly flex-col gap-3">
           <button
-            type="button"
-            className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-md"
-            // onClick={() => setPaciente(paciente)}
-          >Editar</button>
+              type="button"
+              className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+              // onClick={()=> navigate(`/paciente/${id}/editar`)}
+          >
+              Editar
+          </button>
+         
           <button
-            type="button"
-            className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-md"
-            // onClick={handleEliminar}
-          >Eliminar</button>
-        </div>
-    </div>
-  )
+              type="submit"
+              className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+          >
+              Eliminar
+          </button>
+      </td>
+  </tr>
+)
 }
 
 export default Pacientes
