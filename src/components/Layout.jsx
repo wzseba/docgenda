8bb64de16603dec002846cf82fec67cb85f1209d
wzseba <wzseba@gmail.com>
2023-03-16 21:59:01 -0300

    modificacion de estilos se agrago w-fit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,8 +9,8 @@ const Layout = () => {
     <div className='md:flex md:min-h-screen'>
         <aside className='md:w-1/4 bg-gray-700 px-5 py-10'>
             <h2 className='text-4xl font-black text-center text-white'>Bienvenido Doctor Pedro</h2>
-            <Link className={`${location.pathname === '/' ? 'text-gray-900' : 'text-white'} text-2xl block mt-20 hover:text-gray-300`} to='/'>Pacientes</Link>
-            <Link className={`${location.pathname === '/pacientes/nuevo' ? 'text-gray-900' : 'text-white'} text-2xl block mt-2 hover:text-gray-300`} to='/pacientes/nuevo'>Nuevo Paciente</Link>
+            <Link className={`${location.pathname === '/' ? 'text-gray-900' : 'text-white'} text-2xl block mt-20 hover:text-gray-300 w-fit`} to='/'>Pacientes</Link>
+            <Link className={`${location.pathname === '/pacientes/nuevo' ? 'text-gray-900' : 'text-white'} text-2xl block mt-2 hover:text-gray-300 w-fit`} to='/pacientes/nuevo'>Nuevo Paciente</Link>
         </aside>
 
         <main className='md:w-3/4 p-10 md:h-screen'>
