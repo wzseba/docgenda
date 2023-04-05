9d8afcc9e2f307575c85a8715ad8ce11ac3eaf76
wzseba <wzseba@gmail.com>
2023-04-04 22:47:11 -0300

    refactor: path de componente link-pacientes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ const Layout = () => {
     <div className='md:flex md:min-h-screen'>
         <aside className='md:w-1/4 bg-gray-700 px-5 py-10'>
             <h2 className='text-4xl font-black text-center text-white'>Bienvenido Doctor Pedro</h2>
-            <Link className={`${location.pathname === '/' ? 'text-gray-900' : 'text-white'} text-2xl block mt-20 hover:text-gray-300 w-fit`} to='/'>Pacientes</Link>
+            <Link className={`${location.pathname === '/pacientes' ? 'text-gray-900' : 'text-white'} text-2xl block mt-20 hover:text-gray-300 w-fit`} to='/pacientes'>Pacientes</Link>
             <Link className={`${location.pathname === '/pacientes/nuevo' ? 'text-gray-900' : 'text-white'} text-2xl block mt-2 hover:text-gray-300 w-fit`} to='/pacientes/nuevo'>Nuevo Paciente</Link>
         </aside>
 
