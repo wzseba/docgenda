bf46c2de68246b95d93ee63f859ef427e2530962
wzseba <wzseba@gmail.com>
2023-04-04 22:44:52 -0300

    add: componente Sesion

---
+++ src/page/Sesion.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../components/Login'
+
+const Sesion = () => {
+  return (
+    <div>
+      <h1 className='text-center'>
+        INGRESE SUS CREDENCIALES PARA INGRESAR AL SISTEMA
+      </h1>
+      <Login/>
+    </div>
+  )
+}
+
+export default Sesion
