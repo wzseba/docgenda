c4871e56b483e0ea6794ccedb0ecf65e7c33d903
wzseba <wzseba@gmail.com>
2023-04-17 22:38:05 -0300

    refactor: modificacion de extraReducer se cambia por builder

--- src/features/pacientes/pacienteSlice.js
+++ src/features/pacientes/pacienteSlice.js
@@ -12,7 +12,6 @@ export const pacientesAdd = createAsyncThunk(
     async (pacientes,{ rejectWithValue })=>{
         try {
             const response = await axios.post('http://localhost:3001/pacientes',pacientes);
-            console.log('estoy en pacienteSlice',response.data);
             return response.data;
         } catch (error) {
             console.log(error);
@@ -24,50 +23,25 @@ export const pacientesAdd = createAsyncThunk(
 export const pacienteSlice = createSlice({
     name: 'pacientes',
     initialState,
-    reducers:{
-        // addPatient: (state, action) => {
-        //     state.push(action.payload)
-        // },
-        // deletePatient: (state, action) =>{
-        //     const patienteFound = state.find(paciente => paciente.id === action.payload)
-        //     if(patienteFound){
-        //         state.splice(state.indexOf(patienteFound),1)
-        //     }
-        // },
-        // editPatient: (state, action) =>{
-        //     const {id, name, email, telefono, sintomas} = action.payload
-        //     const editPatientFound = state.find(paciente => paciente.id === id)
-        //     if(editPatientFound){
-        //         editPatientFound.name = name,
-        //         editPatientFound.email = email,
-        //         editPatientFound.telefono = telefono,
-        //         editPatientFound.sintomas = sintomas
-        //     }
-        // }
-    },
-    extraReducers:{
-        [pacientesAdd.pending]: (state, action)=>{
-            return {
-                ...state,
-                addPacienteStatus: 'pending',
-                addPacientesError: ''
-            }
-        },
-        [pacientesAdd.fulfilled]: (state, action)=>{
-            return {
-                ...state,
-                pacientes: [action.payload, ...state.pacientes],
-                addPacienteStatus: 'success',
-                addPacientesError: ''
-            }
-        },
-        [pacientesAdd.rejected]: (state, action)=>{
-            return {
-                ...state,
-                addPacienteStatus: 'rejected',
-                addPacientesError: action.payload
-            }
-        }
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder
+        .addCase(pacientesAdd.pending,(state)=>{
+           state.addPacienteStatus = 'loading'
+        })
+        .addCase(pacientesAdd.fulfilled ,(state, action)=>{
+          
+            state.pacientes= [action.payload, ...state.pacientes],
+            state.addPacienteStatus= 'success',
+            state.addPacientesError= ''
+        
+        })
+        .addCase(pacientesAdd.rejected,(state, action)=>{
+            
+            state.addPacienteStatus= 'rejected',
+            state.addPacientesError= action.payload
+            
+        })
     }
 })
 
