55aa7389f2e2c2be93287b6cb9a63e7323b4ad89
wzseba <wzseba@gmail.com>
2023-05-07 23:05:44 -0300

    add: se agrego props para renderizar el contenido

--- src/components/Pacientes.jsx
+++ src/components/Pacientes.jsx
@@ -1,28 +1,16 @@
-import { useDispatch, useSelector } from "react-redux"
 
-// import { deletePatient, editPatient, getPacientes } from "../features/pacientes/pacienteSlice";
-// import { useEffect } from "react";
+const Pacientes = ({paciente}) => {
 
-const Pacientes = () => {
-
-//   const dispatch = useDispatch();
-
-//   const pacientes = useSelector(state => state.paciente);
-//   console.log('componente pacientes ', pacientes);
-
-//   useEffect(()=>{
-//     dispatch(getPacientes());
-//   },[dispatch])
 
 return (
-  <tr className="border-b">
+  <tr key={paciente.id} className="border-b">
       <td className="p-6 space-y-2 text-center">
-          <p className="text-2xl text-gray-800">nombre paciente</p>
+          <p className="text-2xl text-gray-800">{paciente.nombre}</p>
           
       </td>
       <td className="p-6 text-center">
-          <p className="text-gray-600"> <span className="text-gray 800 uppercase font-bold">Email: </span>email paciente </p>
-          <p className="text-gray-600"> <span className="text-gray 800 uppercase font-bold">Tel: </span>telefono paciente</p>
+          <p className="text-gray-600"> <span className="text-gray 800 uppercase font-bold">Email: </span>{paciente.email}</p>
+          <p className="text-gray-600"> <span className="text-gray 800 uppercase font-bold">Tel: </span>{paciente.telefono}</p>
       </td>
       <td className="p-6 flex items-center justify-evenly flex-col gap-3">
           <button
