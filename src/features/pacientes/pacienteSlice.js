9334d1b898e2cc26289225a1971a9042edf178e3
wzseba <wzseba@gmail.com>
2023-05-07 23:08:52 -0300

    delete: se borra funcion thunks para modularizar

--- src/features/pacientes/pacienteSlice.js
+++ src/features/pacientes/pacienteSlice.js
@@ -1,50 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios';
+
 
 const initialState = {
     pacientes: [],
-    addPacienteStatus: '',
-    addPacientesError: ''
+    isLoading: false
 }
-
-export const pacientesAdd = createAsyncThunk(
-    "pacientes/pacientesAdd",
-    async (pacientes,{ rejectWithValue })=>{
-        try {
-            const response = await axios.post('http://localhost:3001/pacientes',pacientes);
-            return response.data;
-        } catch (error) {
-            console.log(error);
-            return rejectWithValue(error.response?.data);
-        }
-    }
-)
 
 export const pacienteSlice = createSlice({
     name: 'pacientes',
     initialState,
-    reducers:{},
-    extraReducers:(builder)=>{
-        builder
-        .addCase(pacientesAdd.pending,(state)=>{
-           state.addPacienteStatus = 'loading'
-        })
-        .addCase(pacientesAdd.fulfilled ,(state, action)=>{
-          
-            state.pacientes= [action.payload, ...state.pacientes],
-            state.addPacienteStatus= 'success',
-            state.addPacientesError= ''
-        
-        })
-        .addCase(pacientesAdd.rejected,(state, action)=>{
-            
-            state.addPacienteStatus= 'rejected',
-            state.addPacientesError= action.payload
-            
-        })
+    reducers:{
+        startLoadingPacientes: (state,/* action */)=>{
+            state.isLoading = true
+        },
+        getAllPacientes: (state, action)=>{
+            state.isLoading = false;
+            state.pacientes = action.payload;
+        },
+        createPaciente: (state, action)=>{
+            console.log('estoy en createPaciente ',action.payload);
+            state.pacientes.push(action.payload);
+        }
     }
+   
 })
 
-export const { addPatient, deletePatient, editPatient } = pacienteSlice.actions
+export const { startLoadingPacientes, getAllPacientes, createPaciente} = pacienteSlice.actions
 
-export default pacienteSlice.reducer
+// export default pacienteSlice.reducer
