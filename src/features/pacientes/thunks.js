883cf26b6d83375aea63921d3aef557ed20aaec1
wzseba <wzseba@gmail.com>
2023-05-07 23:03:15 -0300

    add: se agrego funciones thunks

--- src/features/pacientes/thunks.js
+++ src/features/pacientes/thunks.js
@@ -1,11 +1,27 @@
-import { getAllPacientes, startLoadingPacientes} from './pacienteSlice';
+import { getAllPacientes, startLoadingPacientes, createPaciente} from './pacienteSlice';
 import { pacienteApi } from '../../api/pacienteApi';
 
 export const getPacientes = ()=>{
     return async ( dispatch, getState )=>{
         dispatch( startLoadingPacientes() );
         const { data } = await pacienteApi.get('/pacientes');
-        console.log(data);
+        // console.log(data);
         dispatch( getAllPacientes(data) );
+    }
+}
+
+export const pacientesAdd = (payload)=>{
+    return async(dispatch,getState)=>{
+        try {
+            // dispatch(startLoadingPacientes());
+            // console.log('estoy en thunk ',payload);
+            // const { data } = await pacienteApi.post('/pacientes',payload);
+            
+            dispatch(createPaciente(payload));
+
+        } catch (error) {
+            console.log(error);
+        }
+        
     }
 }
